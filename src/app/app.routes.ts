034a3329c1931e0e3b7e2ba53159f5d8f978d9b1
luckys-lnz <lalucky22@gmail.com>
2025-02-06 23:09:59 +0000

    routes to the content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { FilterContentComponent } from './filter-content/filter-content.component';
+import { FilterBarComponent } from './filter-bar/filter-bar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: FilterBarComponent },
+  { path: 'filter-content/:id', component: FilterContentComponent },
+];
+
